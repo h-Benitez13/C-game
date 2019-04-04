a7d7c528a7c00fd7f450cddeabd33d29ac2e0913
h-Benitez13 <hbenitez3711@gmail.com>
2019-04-04 18:38:37 -0400

    Updates

---
+++ precache-manifest.7fdd7af143b28dcb314dc4fb38b26730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d84f4204c896db572e6271763e6d459",
+    "url": "/C-game/static/media/wow.4d84f420.jpg"
+  },
+  {
+    "revision": "4c57c50cb664561e27f9",
+    "url": "/C-game/static/js/runtime~main.885c4057.js"
+  },
+  {
+    "revision": "c745f1a0d2405cc3040d",
+    "url": "/C-game/static/js/main.bd2035e5.chunk.js"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/C-game/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "c745f1a0d2405cc3040d",
+    "url": "/C-game/static/css/main.53b417b5.chunk.css"
+  },
+  {
+    "revision": "4d04e09b4c08730d9ffb124125802576",
+    "url": "/C-game/index.html"
+  }
+];
